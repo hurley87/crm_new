fa0a11f80bc18c8843eb2f611f8d45ce1422bc25
davidhurley <dhurls99@gmail.com>
2014-06-10 15:22:17 -0400

    finish todays assignment - add contact to crm

--- crm.rb
+++ crm.rb
@@ -25,3 +25,9 @@ end
 get "/contacts/:id/edit" do
 	erb :edit
 end
+
+post '/contacts' do
+  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  @@rolodex.add_contact(new_contact)
+  redirect to('/contacts')
+end
