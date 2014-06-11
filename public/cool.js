202cb5d915ccc0f591211869580c83b7162d6a41
davidhurley <dhurls99@gmail.com>
2014-06-11 12:04:35 -0400

    add some jquery

--- public/cool.js
+++ public/cool.js
@@ -3,44 +3,21 @@
 
 $(document).ready(function() {
 
+	$('.box').find('.other_info').hide();
 
-	//index page
-	$('#user_name').focus();
-	$('#message').hide();
-	var d = new Date();
+	$('.quick_delete').on("mouseover", function() {
+			var div = $(this).closest('.box');
+			div.css("border", "none");
+			$(this).on("mouseout", function() {
+				$(this).closest('.box').css("border", "1px solid white");
+			});
+	});
 
-	$('#user_name').bind("enterKey",function(e){
-   			$('#message').show(2000);
-   			$(this).hide();
-   			$('.question').hide();
-   			$('.name').text($('#user_name').val());
-		});
-		$('#user_name').keyup(function(e){
-		    if(e.keyCode == 13)
-		    {
-		        $(this).trigger("enterKey");
-		    }
-		});
-
-	var month = d.getMonth()+1;
-	var day = d.getDate();
-
-	var output = d.getFullYear() + '/' +
-    (month<10 ? '0' : '') + month + '/' +
-    (day<10 ? '0' : '') + day;
-
-    $('.date').text(output);
-    $('h1').on("load", function() {
-
-    	$(this).fadeIn(3000);
-    });
-
-	var num = 75;
-	$( "#progressbar").progressbar({
-		  value: num
-		});
-
-
-
+	$('.box').on('mouseover', function() {
+			$(this).find('.other_info').show(500);
+	});
+	$('.box').on('mouseleave', function() {
+			$(this).find('.other_info').hide(500);
+	});
 	
 });
